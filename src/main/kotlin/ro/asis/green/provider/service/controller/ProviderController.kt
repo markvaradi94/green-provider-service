70cad39d87150602da5333f95ad8b8e6f8cb43e6
Erdei Mark <erdei_mark@yahoo.com>
2021-05-28 17:50:00 +0300

    features, refactoring, code clean

--- src/main/kotlin/ro/asis/green/provider/service/controller/ProviderController.kt
+++ src/main/kotlin/ro/asis/green/provider/service/controller/ProviderController.kt
@@ -1,45 +1,72 @@
 package ro.asis.green.provider.service.controller
 
 import org.springframework.web.bind.annotation.*
-import ro.asis.green.provider.service.exceptions.ProviderNotFoundException
 import ro.asis.green.provider.service.model.api.dto.ProviderDto
+import ro.asis.green.provider.service.model.entity.Address
+import ro.asis.green.provider.service.model.entity.GreenBag
 import ro.asis.green.provider.service.model.filters.ProviderFilters
-import ro.asis.green.provider.service.model.mappers.ProviderMapper
 import ro.asis.green.provider.service.service.ProviderService
 
 @RestController
 @RequestMapping("providers")
 class ProviderController(
     private val providerService: ProviderService,
-    private val providerMapper: ProviderMapper
 ) {
     @GetMapping
-    fun getAllProviders(filters: ProviderFilters) = providerMapper.toApi(
-        providerService.findAll(filters)
-    )
-
-    @PostMapping
-    fun addProvider(@RequestBody provider: ProviderDto) = providerMapper.toApi(
-        providerService.addProvider(providerMapper.toEntity(provider))
-    )
+    fun getAllProviders(filters: ProviderFilters) = providerService.findAll(filters)
 
     @GetMapping("{providerId}")
-    fun getById(@PathVariable providerId: String) = providerMapper.toApi(
-        providerService.findById(providerId)
-            .orElseThrow { ProviderNotFoundException("Could not find provider") }
-    )
+    fun getProvider(@PathVariable providerId: String) = providerService.findProvider(providerId)
 
-    @PutMapping("{providerId}")
+    @PostMapping
+    fun addProvider(@RequestBody provider: ProviderDto) = providerService.addProvider(provider)
+
+    @PatchMapping("{providerId}")
     fun updateProvider(
         @PathVariable providerId: String,
-        @RequestBody newProvider: ProviderDto
-    ) = providerMapper.toApi(
-        providerService.updateProvider(providerId, providerMapper.toEntity(newProvider))
-    )
+        @RequestBody provider: ProviderDto
+    ) = providerService.patchProvider(providerId, provider)
 
     @DeleteMapping("{providerId}")
-    fun deleteProvider(@PathVariable providerId: String) = providerMapper.toApi(
-        providerService.deleteById(providerId)
-    )
+    fun deleteProvider(@PathVariable providerId: String) = providerService.deleteProvider(providerId)
 
+    @GetMapping("{providerId}/inventory")
+    fun getProviderInventory(@PathVariable providerId: String) = providerService.getProviderInventory(providerId)
+
+    @DeleteMapping("{providerId}/inventory")
+    fun deleteProviderInventory(@PathVariable providerId: String) = providerService.deleteProviderInventory(providerId)
+
+    @PostMapping("{providerId}/inventory")
+    fun addGreenBagToInventory(
+        @PathVariable providerId: String,
+        @RequestBody greenBag: GreenBag
+    ) = providerService.addGreenBagToInventory(providerId, greenBag)
+
+    @PatchMapping("{providerId}/inventory/{greenBagId}")
+    fun patchProviderGreenBag(
+        @PathVariable providerId: String,
+        @PathVariable greenBagId: String,
+        @RequestBody greenBag: GreenBag
+    ) = providerService.patchProviderGreenBag(providerId, greenBagId, greenBag)
+
+    @GetMapping("{providerId}/inventory/{greenBagId}")
+    fun getProviderGreenBag(
+        @PathVariable providerId: String,
+        @PathVariable greenBagId: String
+    ) = providerService.getProviderGreenBag(providerId, greenBagId)
+
+    @DeleteMapping("{providerId}/inventory/{greenBagId}")
+    fun deleteGreenBagFromInventory(
+        @PathVariable providerId: String,
+        @PathVariable greenBagId: String
+    ) = providerService.deleteGreenBagFromInventory(providerId, greenBagId)
+
+    @GetMapping("{providerId}/address")
+    fun getProviderAddress(@PathVariable providerId: String) = providerService.getProviderAddress(providerId)
+
+    @PatchMapping("{providerId}/address")
+    fun patchProviderAddress(
+        @PathVariable providerId: String,
+        @RequestBody address: Address
+    ) = providerService.patchProviderAddress(providerId, address)
 }
--- src/main/kotlin/ro/asis/green/provider/service/service/ProviderService.kt
+++ src/main/kotlin/ro/asis/green/provider/service/service/ProviderService.kt
@@ -2,39 +2,115 @@ package ro.asis.green.provider.service.service
 
 import org.springframework.stereotype.Service
 import ro.asis.green.provider.service.exceptions.ProviderNotFoundException
+import ro.asis.green.provider.service.model.api.dto.ProviderDto
+import ro.asis.green.provider.service.model.entity.Address
+import ro.asis.green.provider.service.model.entity.GreenBag
 import ro.asis.green.provider.service.model.entity.ProviderEntity
 import ro.asis.green.provider.service.model.filters.ProviderFilters
+import ro.asis.green.provider.service.model.mappers.ProviderMapper
 import ro.asis.green.provider.service.repository.ProviderDao
 import ro.asis.green.provider.service.repository.ProviderRepository
 
 @Service
 class ProviderService(
     private val providerRepository: ProviderRepository,
-    private val providerDao: ProviderDao
+    private val providerDao: ProviderDao,
+    private val providerMapper: ProviderMapper
 ) {
-    fun findAll(filters: ProviderFilters) = providerDao.findProviders(filters)
+    fun findAll(filters: ProviderFilters) = providerMapper.toApi(providerDao.findProviders(filters))
 
-    fun findById(providerId: String) = providerRepository.findById(providerId)
-
-    fun addProvider(provider: ProviderEntity) = providerRepository.save(provider)
-
-    fun updateProvider(providerId: String, newProvider: ProviderEntity): ProviderEntity {
-        val providerToUpdate = getOrThrow(providerId)
-        providerToUpdate.name = newProvider.name
-        providerToUpdate.address = newProvider.address
-        providerToUpdate.since = newProvider.since
-        providerToUpdate.description = newProvider.description
-        return providerRepository.save(providerToUpdate)
-    }
-
-    fun deleteById(providerId: String): ProviderEntity {
-        val providerToDelete = getOrThrow(providerId)
-        providerRepository.deleteById(providerId)
-        return providerToDelete
-    }
+    fun findProvider(providerId: String) = providerMapper.toApi(getOrThrow(providerId))
 
     private fun getOrThrow(providerId: String) = providerRepository
         .findById(providerId)
         .orElseThrow { ProviderNotFoundException("Could not find provider with id $providerId") }
+
+    fun addProvider(provider: ProviderDto) = providerMapper.toApi(
+        providerRepository.save(
+            providerMapper.toEntity(provider)
+        )
+    )
+
+    fun patchProvider(providerId: String, newProvider: ProviderDto): ProviderDto {
+        val provider = getOrThrow(providerId)
+        copyClient(newProvider, provider)
+        return providerMapper.toApi(providerRepository.save(provider))
+    }
+
+    private fun copyClient(newProvider: ProviderDto, oldProvider: ProviderEntity) {
+        oldProvider.name = newProvider.name ?: oldProvider.name
+        oldProvider.since = newProvider.since ?: oldProvider.since
+        oldProvider.description = newProvider.description ?: oldProvider.description
+        oldProvider.openingHours = newProvider.openingHours ?: oldProvider.openingHours
+    }
+
+    fun deleteProvider(providerId: String): ProviderDto {
+        val providerToDelete = getOrThrow(providerId)
+        providerRepository.deleteById(providerId)
+        return providerMapper.toApi(providerToDelete)
+    }
+
+    fun getProviderInventory(providerId: String): Set<GreenBag> {
+        val provider = getOrThrow(providerId)
+        return provider.inventory
+    }
+
+    fun deleteProviderInventory(providerId: String): ProviderDto {
+        val provider = getOrThrow(providerId)
+        provider.inventory = mutableSetOf()
+        return providerMapper.toApi(providerRepository.save(provider))
+    }
+
+    fun addGreenBagToInventory(providerId: String, greenBag: GreenBag): ProviderDto {
+        val provider = getOrThrow(providerId)
+        provider.inventory.add(greenBag)
+        return providerMapper.toApi(providerRepository.save(provider))
+    }
+
+    fun patchProviderGreenBag(providerId: String, greenBagId: String, newGreenBag: GreenBag): ProviderDto {
+        val provider = getOrThrow(providerId)
+        val oldGreenBag = provider.inventory.first { it.id.equals(greenBagId, ignoreCase = false) }
+        copyGreenBag(oldGreenBag, newGreenBag)
+        return providerMapper.toApi(providerRepository.save(provider))
+    }
+
+    private fun copyGreenBag(oldGreenBag: GreenBag, newGreenBag: GreenBag) {
+        oldGreenBag.description = newGreenBag.description ?: oldGreenBag.description
+        oldGreenBag.price = newGreenBag.price ?: oldGreenBag.price
+        oldGreenBag.imageUrl = newGreenBag.imageUrl ?: oldGreenBag.imageUrl
+    }
+
+    fun getProviderGreenBag(providerId: String, greenBagId: String): GreenBag {
+        val provider = getOrThrow(providerId)
+        return provider.inventory.first { it.id.equals(greenBagId, ignoreCase = false) }
+    }
+
+    fun deleteGreenBagFromInventory(providerId: String, greenBagId: String): ProviderDto {
+        val provider = getOrThrow(providerId)
+        provider.inventory.removeIf { it.id.equals(greenBagId, ignoreCase = false) }
+        return providerMapper.toApi(providerRepository.save(provider))
+    }
+
+    fun getProviderAddress(providerId: String): Address {
+        val provider = getOrThrow(providerId)
+        return provider.address
+    }
+
+    fun patchProviderAddress(providerId: String, newAddress: Address): ProviderDto {
+        val provider = getOrThrow(providerId)
+        copyAddress(provider.address, newAddress)
+        return providerMapper.toApi(providerRepository.save(provider))
+    }
+
+    private fun copyAddress(oldAddress: Address, newAddress: Address) {
+        oldAddress.city = newAddress.city ?: oldAddress.city
+        oldAddress.zipCode = newAddress.zipCode ?: oldAddress.zipCode
+        oldAddress.streetName = newAddress.streetName ?: oldAddress.streetName
+        oldAddress.streetNumber = newAddress.streetNumber ?: oldAddress.streetNumber
+        oldAddress.building = newAddress.building ?: oldAddress.building
+        oldAddress.staircase = newAddress.staircase ?: oldAddress.staircase
+        oldAddress.floor = newAddress.floor ?: oldAddress.floor
+        oldAddress.flat = newAddress.flat ?: oldAddress.flat
+    }
 
 }
